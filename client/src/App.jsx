963626973558e2356ebfea8de72c476b826cdb9c
kkartikeya22 <kkartikeya22@iitk.ac.in>
2024-03-15 09:35:14 +0530

    header component created using flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import SigIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/sign-in' element={<SigIn/>}/>
-        <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SigIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
     </BrowserRouter>
   )
